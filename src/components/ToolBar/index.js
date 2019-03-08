5c6ba2fde8540a39c19195a34011c5e750fb92cc
Albert Moreira <albertm@take.net>
2019-03-08 13:20:02 -0300

    Changing icons for svg inline0

--- src/components/ToolBar/index.js
+++ src/components/ToolBar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './ToolBar.scss';
 import AnswerModels from '../../models/AnswerModels';
+import SVG from 'react-inlinesvg';
 
 import textImg from './img/text.svg';
 import imageImg from './img/image.svg';
@@ -14,45 +15,42 @@ import jsonImg from './img/json.png';
 class ToolBar extends Component {
     render() {
         return (
-            <div className="ButtonBar">
-                <img
-                    onClick={() => this.props.addAnswer(AnswerModels.text)}
-                    alt="Texto"
-                    title="Texto"
-                    src={textImg}
-                />
-                <img
-                    onClick={() => this.props.addAnswer(AnswerModels.menu)}
-                    alt="Menu"
-                    title="Menu"
-                    src={menuImg}
-                />
-                <img
-                    onClick={() => this.props.addAnswer(AnswerModels.quickReply)}
-                    alt="Quick Reply"
-                    title="Quick Reply"
-                    src={quickReplyImg}
-                />
-                <img
-                    onClick={() => this.props.addAnswer(AnswerModels.carousel)}
-                    alt="Carrossel"
-                    title="Carrossel"
-                    src={carouselImg}
-                />
-                <img
-                    onClick={() => this.props.addAnswer(AnswerModels.image)}
-                    alt="Imagem"
-                    title="Imagem"
-                    src={imageImg}
-                />
-                <img
-                    onClick={() => this.props.addAnswer(AnswerModels.track)}
-                    alt="Track"
-                    title="Track"
-                    src={trackImg}
-                />
+            <div className="ToolBar">
+                <button onClick={() => this.props.addAnswer(AnswerModels.text)} >
+                    <SVG src={textImg} className="button">
+                        Texto
+                    </SVG>
+                </button>
+                <button onClick={() => this.props.addAnswer(AnswerModels.menu)} >
+                    <SVG src={menuImg} className="button">
+                        Menu
+                </SVG>
+                </button>
+                <button onClick={() => this.props.addAnswer(AnswerModels.quickReply)} >
+                    <SVG src={quickReplyImg} className="button">
+                        QuickReply
+                </SVG>
+                </button>
+                <button onClick={() => this.props.addAnswer(AnswerModels.carousel)} >
+                    <SVG src={carouselImg} className="button">
+                        Carousel
+                </SVG>
+                </button>
+                <button onClick={() => this.props.addAnswer(AnswerModels.image)} >
+                    <SVG src={imageImg} className="button">
+                        Imagem
+                </SVG>
+                </button>
+                <button onClick={() => this.props.addAnswer(AnswerModels.track)} >
+                    <SVG src={trackImg} className="button">
+                        Track
+                </SVG>
+                </button>
+
+
                 {(this.props.answers.length === 0 || this.props.answers[0].type !== 'ai') && (
                     <img
+                        className="btn"
                         onClick={() => this.props.addAnswer(AnswerModels.ai)}
                         alt="IA"
                         title="IA"
@@ -60,6 +58,7 @@ class ToolBar extends Component {
                     />
                 )}
                 <img
+                    className="btn"
                     onClick={() => this.props.addAnswer(AnswerModels.json)}
                     alt="Json"
                     title="Json"
